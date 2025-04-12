94f7b97d061cc6a92f6c77d64b349f57b6a68836
Kyuofox <kyuofox@gmail.com>
2025-04-12 05:07:19 +0000

    sm8650-common: Allow sched_get_priority_(min/max) syscall for qsap_location
    I qsap_location: cannot open file:/data/vendor/location/gps.prop
    W qsap_location: libminijail[5445]: failed to get path of fd 3: No such file or directory
    W qsap_location: libminijail[5445]: allowing syscall: connect
    W qsap_location: libminijail[5445]: allowing syscall: fcntl
    W qsap_location: libminijail[5445]: allowing syscall: sendto
    W qsap_location: libminijail[5445]: allowing syscall: socket
    W qsap_location: libminijail[5445]: allowing syscall: writev
    W qsap_location: libminijail[5445]: compile_file: <fd>(84): syscall connect redefined here
    W qsap_location: libminijail[5445]: compile_file: <fd>(38): previous definition here
    W qsap_location: libminijail[5445]: compile_file: <fd>(101): syscall getsockname redefined here
    W qsap_location: libminijail[5445]: compile_file: <fd>(86): previous definition here
    W qsap_location: libminijail[5445]: compile_file: <fd>(110): nonexistent syscall 'chown'
    W qsap_location: libminijail[5445]: compile_file: <fd>(112): nonexistent syscall 'lchown'
    W qsap_location: libminijail[5445]: compile_file: <fd>(113): nonexistent syscall 'mmap2'
    W qsap_location: libminijail[5445]: compile_file: <fd>(114): nonexistent syscall 'fstat64'
    W qsap_location: libminijail[5445]: compile_file: <fd>(115): nonexistent syscall 'fstatat64'
    W qsap_location: libminijail[5445]: compile_file: <fd>(116): nonexistent syscall '_llseek'
    E qsap_location: libminijail[5445]: blocked syscall: sched_get_priority_min
    E qsap_location: libminijail[5445]: blocked syscall: sched_get_priority_max
    
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,9 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libhidlbase_shim.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
+    'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
+        .add_line_if_missing('sched_get_priority_min: 1')
+        .add_line_if_missing('sched_get_priority_max: 1'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
